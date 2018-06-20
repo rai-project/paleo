5982df391984192d3042bdb5dd84d90de8b88fbd
Abdul Dakkak <dakkak@illinois.edu>
2018-06-20 09:24:06 -0500

    work

--- scripts/exp_layers.sh
+++ scripts/exp_layers.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
-NET_FILE=$1
-OUT_FILE=$2
+DEVICE_NAME=$1
+NET_FILE=$2
+OUT_FILE=$3
 
 PPP_COMP=0.62
 
@@ -20,10 +21,12 @@ echo '\n\n' >> $OUT_FILE
     --direction=forward \
     --executor=cudnn \
     --ppp_comp=$PPP_COMP \
+    --device_name=$DEVICE_NAME \
     >> $OUT_FILE
 ./paleo.sh profile $NET_FILE \
     --direction=forward \
     --executor=tensorflow \
+    --device_name=$DEVICE_NAME \
     >> $OUT_FILE
 
 echo '\n\n' >> $OUT_FILE
@@ -32,11 +35,13 @@ echo '\n\n' >> $OUT_FILE
     --gradient_wrt=data \
     --executor=cudnn \
     --ppp_comp=$PPP_COMP \
+    --device_name=$DEVICE_NAME \
     >> $OUT_FILE
 ./paleo.sh profile $NET_FILE \
     --direction=backward \
     --gradient_wrt=data \
     --executor=tensorflow \
+    --device_name=$DEVICE_NAME \
     >> $OUT_FILE
 
 echo '\n\n' >> $OUT_FILE
@@ -45,11 +50,13 @@ echo '\n\n' >> $OUT_FILE
     --gradient_wrt=filter \
     --executor=cudnn \
     --ppp_comp=$PPP_COMP \
+    --device_name=$DEVICE_NAME \
     >> $OUT_FILE
 ./paleo.sh profile $NET_FILE \
     --direction=backward \
     --gradient_wrt=filter \
     --executor=tensorflow \
+    --device_name=$DEVICE_NAME \
     >> $OUT_FILE
 
 popd
--- scripts/exp_layers_all.sh
+++ scripts/exp_layers_all.sh
@@ -1,14 +1,21 @@
 #! /bin/sh
 
+DEVICE_NAME=$1
+
+if [ ! -d "../results/$DEVICE_NAME" ]; then
+    mkdir -p ../results/$DEVICE_NAME
+    echo "made dir results/$DEVICE_NAME"
+fi
+
 # AlexNet
-./exp_layers.sh nets/alex_v2.json results/layers_alex_v2
+./exp_layers.sh $DEVICE_NAME nets/alex_v2.json results/$DEVICE_NAME/layers_alex_v2
 
 # VGG16
-./exp_layers.sh nets/vgg16.json results/layers_vgg16
+./exp_layers.sh $DEVICE_NAME nets/vgg16.json results/$DEVICE_NAME/layers_vgg16
 
 # resnet50
-./exp_layers.sh nets/resnet50.json results/layers_resnet50
+./exp_layers.sh $DEVICE_NAME nets/resnet50.json results/$DEVICE_NAME/layers_resnet50
 
 # Overfeat
-# ./exp_layers.sh nets/overfeat.json results/layers_overfeat
+# ./exp_layers.sh $DEVICE_NAME nets/overfeat.json results/$DEVICE_NAME/layers_overfeat
 
